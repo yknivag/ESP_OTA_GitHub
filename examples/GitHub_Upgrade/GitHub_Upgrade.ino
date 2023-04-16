384cb4e52d8e110e6a2c5670db8f667729923c2b
lademeister <lademeister@gmail.com>
2023-04-16 15:51:30 +0200

    improve example to read WiFi credentials from .txt
    improve example to read WiFi credentials from a text file ('known_wifis.txt') that can be uploaded to SPIFFS memory, so that the user does not have to publish a code on his public GitHub repository that contains his private WiFi credentials. instead add the possibility to upload a text file to SPIFFS that contains the WiFi credentials, und the code will dynamically load the credentials from the .txt file that is stored locally on the device.

--- examples/GitHub_Upgrade/GitHub_Upgrade.ino
+++ examples/GitHub_Upgrade/GitHub_Upgrade.ino
@@ -1,19 +1,16 @@
-/*
-  GitHub_Upgrade.ino - Example for ESP library for auto updating code from GitHub releases.
-  Created by Gavin Smalley, November 13th 2019.
-  Released under the LGPL v2.1.
-  It is the author's intention that this work may be used freely for private
-  and commercial use so long as any changes/improvements are freely shared with
-  the community under the same terms.
-*/
-
 #include <ESP8266WiFi.h>
 #include <FS.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 #ifndef STASSID
-#define STASSID "ssid"
-#define STAPSK  "password"
+#define STASSID "IdoNOTwantToShowMySSIDtoApublicGitHibRepository"
+#define STAPSK  "mySSIDandPasswordAreStoredOnAtextfile_locally_on_ESP8266"
 #endif
+
+//variables to read a wifi SSID and wifi password from a text file (named known_wifis.txt) which is stored locally on your ESP8266, so that the publicly visible code on the GitHub repository doesn't contain your wifi credentials
+String SSID_txt = "";
+String PASS_txt = "";
 
 // A single, global CertStore which can be used by all
 // connections.  Needs to stay live the entire time any of
@@ -22,67 +19,131 @@
 BearSSL::CertStore certStore;
 
 /* Set up values for your repository and binary names */
-#define GHOTA_USER "username"
-#define GHOTA_REPO "repository"
+#define GHOTA_USER "lademeister"
+#define GHOTA_REPO "ESP_OTA_GitHub"
 #define GHOTA_CURRENT_TAG "0.0.0"
-#define GHOTA_BIN_FILE "sketchname.ino.d1_mini.bin"
-#define GHOTA_ACCEPT_PRERELEASE 0
+#define GHOTA_BIN_FILE "GitHub_Upgrade.ino.generic.bin"
+
+#define GHOTA_ACCEPT_PRERELEASE 0 //if set to 1 we will also update if a release of the github repository is set as 'prereelease'. Ignore prereleases if set to 0.
 
 #include <ESP_OTA_GitHub.h>
-
-void handle_upgade() {
-	// Initialise Update Code
-	//We do this locally so that the memory used is freed when the function exists.
-	ESPOTAGitHub ESPOTAGitHub(&certStore, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
-	
-	Serial.println("Checking for update...");
-    if (ESPOTAGitHub.checkUpgrade()) {
-		Serial.print("Upgrade found at: ");
-		Serial.println(ESPOTAGitHub.getUpgradeURL());
-		if (ESPOTAGitHub.doUpgrade()) {
-			Serial.println("Upgrade complete."); //This should never be seen as the device should restart on successful upgrade.
-		} else {
-			Serial.print("Unable to upgrade: ");
-			Serial.println(ESPOTAGitHub.getLastError());
-		}
-    } else {
-		Serial.print("Not proceeding to upgrade: ");
-		Serial.println(ESPOTAGitHub.getLastError());
-    }
-}
+// Initialise Update Code
+ESPOTAGitHub ESPOTAGitHub(&certStore, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
 
 void setup() {
-	// Start serial for debugging (not used by library, just this sketch).
-	Serial.begin(115200);
-	
-	// Start SPIFFS and retrieve certificates.
-	SPIFFS.begin();
-	int numCerts = certStore.initCertStore(SPIFFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
-	Serial.print(F("Number of CA certs read: "));
-	Serial.println(numCerts);
-	if (numCerts == 0) {
-		Serial.println(F("No certs found. Did you run certs-from-mozill.py and upload the SPIFFS directory before running?"));
-		return; // Can't connect to anything w/o certs!
-	}
+  // Start serial for debugging (not used by library, just this sketch).
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
 
-	// Connect to WiFi
-	Serial.print("Connecting to WiFi... ");
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(STASSID, STAPSK);
-	if ((WiFi.status() != WL_CONNECTED)) {
-		Serial.print("... ");
-	}
-	Serial.println();
+  Serial.println();
+  Serial.println("================================================================================");
+  Serial.println("|                                                                              |");
+  Serial.println("|                  Welcome to the ESP GitHub OTA Update TEST                   |");
+  Serial.println("|                =============================================                 |");
+  Serial.print("|    Version:    ");
+  Serial.print(GHOTA_CURRENT_TAG);
+  Serial.println("                                                         |");
+  Serial.println("|                                                                              |");
+  Serial.println("================================================================================");
+  Serial.println();
+  Serial.println();
+
+  // Start SPIFFS and retrieve certificates.
+  SPIFFS.begin();
+  int numCerts = certStore.initCertStore(SPIFFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+  Serial.println();
+  Serial.println();
+  Serial.print(F("Number of CA certs read: "));
+  Serial.println(numCerts);
+  if (numCerts == 0) {
+    Serial.println(F("No certs found. Did you run certs-from-mozilla.py on your computer and upload the file 'certs.ar' to the SPIFFS memory section on ESP8266 before flashing this sketch to the device?"));
+    
+    return; // Can't connect to anything w/o certs!
+  }
+
+  Serial.print("We use OTA update from a public GitHub repository. As we do not want to upload sketches that contain Wifi credentials, \nwe instead upload a file 'known_wifis.txt' to the SPIFFS memory section\non ESP8266 and read our credentials from there, so no one can see them in the git repositiry.");
+
+   // Open the known wifi text file from text file on SPIFFS for reading
+  File file = SPIFFS.open("/known_wifis.txt", "r");
   
+  if (file) {
+    // Loop through each line in the file
+    while (file.available()) {
+      String line = file.readStringUntil('\n');
+      
+//      // Extract the content before and after the first space character
+//      int value = line.substring(0, line.indexOf(' ')).toInt();
+//      String text = line.substring(line.indexOf(' ') + 1);
+
+      // Extract the content before and after the first '/' character (which is used to separate SSID and password within one line in the text file
+      SSID_txt = line.substring(0, line.indexOf('/'));
+      PASS_txt = line.substring(line.indexOf('/') + 1);
+      
+      // Print the extracted values
+      Serial.println("Reading WiFi credentials from textfile in SPIFFS memory on ESP8266:");
+      Serial.print("SSID: ");
+      Serial.println(SSID_txt);
+      Serial.print("Password: ");
+      Serial.println(PASS_txt);
+
+      Serial.print("old WiFi SSID from sketch: ");
+      Serial.println(STASSID);
+      Serial.print("old WiFi Password from sketch: ");
+      Serial.println(STAPSK);
+      
+      //setting the wifi credentials to the ones that we just read from the text file to use them in our sketch:
+      #undef STASSID
+      #define STASSID SSID_txt
+      #undef STAPSK
+      #define STAPSK PASS_txt
+
+      Serial.print("new set WiFi SSID: ");
+      Serial.println(STASSID);
+      Serial.print("new set WiFi Password: ");
+      Serial.println(STAPSK);
+    }
+    
+    // Close the file
+    file.close();
+  }
+  else{
+    Serial.println("file 'known_wifis.txt' was not found on SPIFFS memory of ESP8266, not not using any presets for wifi credentials that are set outside this sketch. Using sketch credentials instead.");
+  }
+
+
+  // Connect to WiFi
+  Serial.print("Connecting to WiFi... ");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+  if ((WiFi.status() != WL_CONNECTED)) {
+    Serial.print("... ");
+  }
+  Serial.print("..Connected.");
+  Serial.println();
+
     /* This is the actual code to check and upgrade */
-    handle_upgade();
+    Serial.println("Checking if an OTA update is available on GitHub...");
+    if (ESPOTAGitHub.checkUpgrade()) {
+    Serial.print("Upgrade found at: ");
+    Serial.println(ESPOTAGitHub.getUpgradeURL());
+    if (ESPOTAGitHub.doUpgrade()) {
+      Serial.println("Upgrade complete."); //This should never be seen as the device should restart on successful upgrade.
+    } else {
+      Serial.print("Unable to upgrade: ");
+      Serial.println(ESPOTAGitHub.getLastError());
+    }
+    } else {
+    Serial.print("Not proceeding to upgrade: ");
+    Serial.println(ESPOTAGitHub.getLastError());
+    }
     /* End of check and upgrade code */
-  
-	// Your setup code goes here
+
+  // Your setup code goes here
 
 }
 
 void loop () {
-	// Your loop code goes here
+  // Your loop code goes here
 
-}
+} 
